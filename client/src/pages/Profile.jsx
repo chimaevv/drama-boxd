8785aae9bd22601991b1feb1f9c1a1ddbabcd635
callmenami <152575413+callmenami@users.noreply.github.com>
2024-09-21 22:49:47 -0400

    added routes and creaded pages

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className="bg-gray-100 p-4">
+    <h1 className="text-2xl font-bold">Welcome to the App</h1>
+  </div>
+  )
+}
