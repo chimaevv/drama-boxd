42ce0ca89d1172c461bfcd61e843fc43e4346b68
callmenami <152575413+callmenami@users.noreply.github.com>
2024-09-25 23:07:50 -0400

    added header components

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className = 'bg-pink-300' >
       <div className ="flex justify-between items-center max-w-6xl mx-auto p-3">
+      <Link to ="/">
       <h1 className ='font-bold' >DramaBoxd</h1>
-      <ul className = 'flex gap-7'>
+      </Link>
+      <ul className = 'flex gap-4'>
         <li> Home</li>
         <li>Genre</li>
         <li>Drama Details</li>
